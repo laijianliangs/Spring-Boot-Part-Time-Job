de11503c409f500064525998f9fa959a046e3f33
赖建梁 <1639662642@qq.com>
2019-09-28 11:21:16 +0800

    inti projects

---
+++ src/test/java/com/atljl/parttime/ParttimeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atljl.parttime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParttimeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
